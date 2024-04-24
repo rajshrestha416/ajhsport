4a9031ccfd15f789c958b90424e3d8eba3065564
rajshrestha416 <rajshrestha416@gmail.com>
2024-04-24 18:00:04 +0545

    Optimised Booking Code

--- src/modules/booking/booking.controller.js
+++ src/modules/booking/booking.controller.js
@@ -4,11 +4,14 @@ const httpStatus = require('http-status');
 const Joi = require('joi');
 const { default: Stripe } = require('stripe');
 const coachingModel = require('../coaching/coaching.model');
-const stripe = require('stripe')('sk_test_51OlkLnGxYrincDprqd6Ur9s5Svo1Aqe03SD48vkm6AdWkpt8bItk0g9JhKAlIz6PNMSaMOlbfBNQRwpnEzKkqmsw00R4Y5bQg8');
+console.log('ev',process.env.STRIPE_KEY)
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const bookingJoiSchema = Joi.object({
-  user: Joi.string().required(),
-  lesson: Joi.string().required()
+  lesson: Joi.string().required(),
+  lesson_name: Joi.string().required(),
+  lesson_type: Joi.string().required(),
+  price: Joi.number().required()
 });
 
 //payment
@@ -17,14 +20,24 @@ exports.createPaymentIntent = async (req, res, next) => {
     // const customer = await stripe.customers.create({
     //   email: 'testingPayment@gmail.com'
     // })
+    const { error } = await bookingJoiSchema.validate(req.body);
+    if (error) {
+      sendErrorResponse(res, httpStatus.BAD_REQUEST, 'Booking Failed', {}, error.message);
+    }
 
-    const {lesson, lesson_name, lesson_type, price} = req.body;
-    const getLesson = await coachingModel.findById(lesson)
-    if(!getLesson) return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Lesson Not Found');
+    const { lesson, lesson_name, lesson_type, price } = req.body;
+    const getBooking = await Booking.findOne({
+      lesson: lesson,
+      user: req.user._id
+    });
+    if (getBooking) return sendErrorResponse(res, httpStatus.CONFLICT, 'Booking Already Done');
 
-    const lessonPriceIndex = getLesson.price.findIndex(val => val.name === lesson_name)
+    const getLesson = await coachingModel.findById(lesson);
+    if (!getLesson) return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Lesson Not Found');
+
+    const lessonPriceIndex = getLesson.price.findIndex(val => val.name === lesson_name);
     //check Lesson Name
-    if(lessonPriceIndex === -1) return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Lesson Price Not found');
+    if (lessonPriceIndex === -1) return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Lesson Price Not found');
 
     const booking = await Booking.create({
       user: req.user._id,
@@ -32,7 +45,7 @@ exports.createPaymentIntent = async (req, res, next) => {
       price,
       lesson_name,
       lesson_type,
-    })
+    });
     const lineItems = [
       {
         price_data: {
@@ -45,13 +58,13 @@ exports.createPaymentIntent = async (req, res, next) => {
         quantity: 1
 
       }
-    ]
-    
+    ];
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "http://localhost:8888/booking/payment-success"+booking._id,
+      success_url: `http://localhost:${process.env.PORT}/booking/payment-success/${booking._id}`,
       cancel_url: "http://localhost:3000/failure",
     });
     // if(session.id){
@@ -72,68 +85,14 @@ exports.createPaymentIntent = async (req, res, next) => {
 
 exports.successPayment = async (req, res, next) => {
   try {
-    const booking = await Booking.findById(req.params.id)
-    if(!booking) return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Booking Not Found');
-    booking.is_payed = true
-    await booking.save()
-    return res.redirect(301, 'http://localhost:3000/success')
+    const booking = await Booking.findById(req.params.id);
+    if (!booking) return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Booking Not Found');
+    booking.is_payed = true;
+    await booking.save();
+    return res.redirect(301, 'http://localhost:3000/success');
   } catch (error) {
     next(error);
   }
-}
-
-// @route POST booking/
-// @desc add booking
-exports.addBooking = async (req, res) => {
-  upload.single('image')(req, res, async error => {
-
-    if (error) {
-      return sendErrorResponse(res, httpStatus.INTERNAL_SERVER_ERROR, 'Error during file upload');
-    }
-    try {
-      const { error } = bookingJoiSchema.validate(req.body);
-
-      if (error) {
-        sendErrorResponse(res, httpStatus.BAD_REQUEST, 'Failed to add booking', {}, error.message);
-      }
-
-      const checkBookingExists = await Booking.findOne({ title: req.body.title, is_deleted: false });
-      if (checkBookingExists) {
-        sendErrorResponse(res, httpStatus.CONFLICT, 'Your Booking Already Exist', {});
-      }
-
-      //add path
-      req.body.image = req.file.path.split('uploads')[1];
-
-      const booking = await Booking.create({ ...req.body });
-      return sendSuccessResponse(res, httpStatus.OK, 'Booking Added', booking);
-      // })
-    } catch (error) {
-      return sendErrorResponse(res, httpStatus.INTERNAL_SERVER_ERROR, 'Failed to add booking', error.message);
-    }
-  });
-};
-
-// @route PUT booking/:id
-// @desc update booking by ID
-exports.updateBooking = async (req, res) => {
-  upload.single('image')(req, res, async error => {
-    try {
-      //add path
-      if (req.file) {
-        req.body.image = req.file.path.split('uploads')[1];
-      }
-
-      const updatedBooking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      if (!updatedBooking) {
-        return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Booking not found');
-      }
-      return sendSuccessResponse(res, httpStatus.OK, 'Booking Updated', updatedBooking);
-      // })
-    } catch (error) {
-      return sendErrorResponse(res, httpStatus.INTERNAL_SERVER_ERROR, 'Failed to update booking', error.message);
-    }
-  });
 };
 
 // @route GET booking/
@@ -142,15 +101,26 @@ exports.getAllBookings = async (req, res, next) => {
   try {
     let { page, limit, selectQuery, searchQuery, sortQuery, populate } = parseFilters(req);
     searchQuery = { is_deleted: false };
-    if (req.query.search) {
-      searchQuery = {
-        title: {
-          $regex: req.query.search,
-          $options: 'i',
-        },
-        ...searchQuery,
-      };
-    }
+    populate = [
+      {
+        path: 'user',
+        select: 'firstname lastname email contact'
+      },
+      {
+        path: 'lesson',
+        select: 'title image description slug price'
+      }
+    ];
+    selectQuery = '-__v -is_deleted -updatedAt';
+    // if (req.query.search) {
+    //   searchQuery = {
+    //     title: {
+    //       $regex: req.query.search,
+    //       $options: 'i',
+    //     },
+    //     ...searchQuery,
+    //   };
+    // }
     const bookings = await sendResponse(Booking, page, limit, sortQuery, searchQuery, selectQuery, populate, next);
     return sendSuccessResponse(res, httpStatus.OK, 'Booking fetched', { bookings, randomBookings });
   } catch (error) {
@@ -158,32 +128,29 @@ exports.getAllBookings = async (req, res, next) => {
   }
 };
 
-// @route GET booking/:id
-// @desc get booking by ID
-exports.getBookingById = async (req, res) => {
+// @route GET booking/my-booking
+// @desc get my booking
+exports.getMyBooking = async (req, res) => {
   try {
-    const bookingId = req.params.id;
-    const booking = await Booking.findById(bookingId).select('-__v -is_deleted -updatedAt')
-      .lean();
-    if (!booking) {
-      return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Booking not found');
-    }
-    return sendSuccessResponse(res, httpStatus.OK, 'Booking fetched', booking);
+    let { page, limit, selectQuery, searchQuery, sortQuery, populate } = parseFilters(req);
+    searchQuery = {
+      user: req.user._id,
+      is_deleted: false
+    };
+    populate = [
+      {
+        path: 'user',
+        select: 'firstname lastname email contact'
+      },
+      {
+        path: 'lesson',
+        select: 'title image description slug price'
+      }
+    ];
+    selectQuery = '-__v -is_deleted -updatedAt';
+    const bookings = await sendResponse(Booking, page, limit, sortQuery, searchQuery, selectQuery, populate, next);
+    return sendSuccessResponse(res, httpStatus.OK, 'Booking fetched', bookings);
   } catch (error) {
     return sendErrorResponse(res, httpStatus.INTERNAL_SERVER_ERROR, 'Failed to fetch booking', error.message);
-  }
-};
-
-// @route DELETE booking/:id
-// @desc delete booking by ID
-exports.deleteBooking = async (req, res) => {
-  try {
-    const deletedBooking = await Booking.findByIdAndUpdate(req.params.id, { $set: { is_deleted: true } });
-    if (!deletedBooking) {
-      return sendErrorResponse(res, httpStatus.NOT_FOUND, 'Booking not found');
-    }
-    return sendSuccessResponse(res, httpStatus.OK, 'Booking Deleted');
-  } catch (error) {
-    return sendErrorResponse(res, httpStatus.INTERNAL_SERVER_ERROR, 'Failed to delete booking', error.message);
   }
 };
--- src/modules/booking/booking.route.js
+++ src/modules/booking/booking.route.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const controller = require('./booking.controller');
 const { verifyUser } = require('../../middleware/auth');
 
+// router.post('/', controller.addData);
 router.post('/create-intent', verifyUser, controller.createPaymentIntent);
 router.get('/payment-success/:id', controller.successPayment);
 // router.post('/', verifyUser, controller.addBooking);
 // router.put('/:id', controller.updateBlog);
 router.get('/', controller.getAllBookings);
-router.get('/:id', controller.getBookingById);
-router.delete('/:id', verifyUser, controller.deleteBooking);
+router.get('/my-booking', verifyUser, controller.getMyBooking);
 
 module.exports = router;
