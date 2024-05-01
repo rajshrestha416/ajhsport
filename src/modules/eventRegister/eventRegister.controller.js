35b19faac6684ec91f585da69b85413e341296ac
rajshrestha416 <rajshrestha416@gmail.com>
2024-05-01 14:18:52 +0545

    Update User Event Status

--- src/modules/eventRegister/eventRegister.controller.js
+++ src/modules/eventRegister/eventRegister.controller.js
@@ -20,19 +20,19 @@ exports.addEventRegistration = async (req, res, next) => {
         if (error) {
             return sendErrorResponse(res, httpStatus.BAD_REQUEST, error.message);
         }
-        console.log('req.user._id', req.user._id)
+        console.log('req.user._id', req.user._id);
         //check Duplicates
         const is_duplicate = await EventRegistration.findOne({
             event: req.body.event,
             user: req.user._id,
-            isDeleted: false 
+            isDeleted: false
         });
 
         if (is_duplicate) {
             return sendErrorResponse(res, httpStatus.CONFLICT, 'Already Registered');
         }
 
-        const event = await EventRegistration.create({...req.body, user: req.user._id});
+        const event = await EventRegistration.create({ ...req.body, user: req.user._id });
         return sendSuccessResponse(res, httpStatus.OK, 'EventRegistration Added', event);
     } catch (error) {
         console.log("err", error);
@@ -43,27 +43,27 @@ exports.addEventRegistration = async (req, res, next) => {
 exports.getEventRegistrations = async (req, res, next) => {
     try {
         let { page, limit, searchQuery, selectQuery, sortQuery, populate } = parseFilters(req);
-        if(req.query.event){
-            const event = await Event.findById(req.query.event)
-            if(!event){
+        if (req.query.event) {
+            const event = await Event.findById(req.query.event);
+            if (!event) {
                 return sendErrorResponse(res, httpStatus.CONFLICT, 'Event Not Found');
             }
             searchQuery = {
                 ...searchQuery,
                 event: event._id
-            }
+            };
         }
         populate = [
             {
-              path: 'user',
-              select: 'firstname lastname email contact'
+                path: 'user',
+                select: 'firstname lastname email contact'
             },
             {
-              path: 'event',
-              select: 'eventName eventSlug eventDescription startDate endData startTime endTime occurrence location'
+                path: 'event',
+                select: 'eventName eventSlug eventDescription startDate endData startTime endTime occurrence location'
             }
-          ];
-        selectQuery = '-isDeleted -__v'
+        ];
+        selectQuery = '-isDeleted -__v';
         const eventRegistrations = await sendResponse(EventRegistration, page, limit, sortQuery, searchQuery, selectQuery, populate);
         return sendSuccessResponse(res, httpStatus.OK, 'Registered Users', eventRegistrations);
     } catch (error) {
@@ -73,22 +73,36 @@ exports.getEventRegistrations = async (req, res, next) => {
 
 exports.getEventRegistration = async (req, res, next) => {
     try {
-        const id = req.params.id
+        const id = req.params.id;
 
         const event = await EventRegistration.findOne({
             _id: id,
             isDeleted: false
         }).select('-isDeleted -__v').populate([
             {
-              path: 'user',
-              select: 'firstname lastname email contact'
+                path: 'user',
+                select: 'firstname lastname email contact'
             },
             {
-              path: 'event',
-              select: 'eventName eventSlug eventDescription startDate endData startTime endTime occurrence location'
+                path: 'event',
+                select: 'eventName eventSlug eventDescription startDate endData startTime endTime occurrence location'
             }
-          ])
+        ]);
         return sendSuccessResponse(res, httpStatus.OK, 'EventRegistration', event);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.updateUserEventStatus = async (req, res, next) => {
+    try {
+        const { eventId, userId } = req.params;
+        const registeredEvent = await EventRegistration.findOneAndUpdate({
+            event: eventId,
+            user: userId,
+            isDeleted: false
+        }, req.body, { new: true});
+        return sendSuccessResponse(res, httpStatus.OK, 'Registration Updated', registeredEvent);
     } catch (error) {
         next(error);
     }
